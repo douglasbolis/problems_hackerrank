e2376f511947710617fe44be50cbc3635743f0d7
douglasbolis <doulgas.lima@xdevel.com.br>
2015-04-09 15:42:53 -0300

    Iniciado o problema Lonely Integer

---
+++ algorithms/lonely_integer.js
@@ -0,0 +1,31 @@
+function processData(input) {
+    var temPar = false,
+        achou = false,
+        i = 0,
+        mat = input.split('\n');
+        lst = mat[1].split(' ');
+
+
+
+    while ((i < lst.length) && (!achou)) {
+        temPar = false;
+        achou = false;
+
+        for (var j = 0; j < lst.length; j++) {
+            if (i != j) {
+                if (lst[i] == lst[j]) {
+                    temPar = true
+                }
+            }
+        }
+        if (!temPar) {
+            achou = true
+        }
+        i++
+    }
+    return lst[i-1]
+}
+
+var lst = "1\n1";
+
+console.log(processData(lst));
