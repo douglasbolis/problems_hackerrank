7a7d9c7decaffadc0c5554fd830ef69ba23aec4d
douglasbolis <douglas.lima@xdevel.com.br>
2015-04-16 01:39:41 -0300

    Ajustando detalhes (lógica) final do problema Chocolate Feast.

--- algorithms/chocolate_feast.js
+++ algorithms/chocolate_feast.js
@@ -1,8 +1,20 @@
 function processData(input) {
-    var lstCase = input.split("\n"), auxA;
+    var lstCase = input.split("\n"), lstAux;
+    var res, auxB;
 
-    console.log(input);
-    console.log(lstCase);
+    for (var i = 1; i <= lstCase[0]; i++) {
+        lstAux = lstCase[i].split(" ");
+        res = parseInt(lstAux[0]/lstAux[1]);
+
+        //TODO terminar a lógica de quantos chocolates Brenno consegue comer (kkkk...).
+        if (res >= lstAux[2]) {
+            res += parseInt(res/lstAux[2])
+        }
+        console.log(res);
+    }
+
+    /*console.log(input);
+    console.log(lstCase);*/
 }
 
 var str = "3\n10 2 5\n12 4 4\n6 2 2";
